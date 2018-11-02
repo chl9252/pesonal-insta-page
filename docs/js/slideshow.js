ecf496156232c7b878eb1313f6d0fd6332391dda
chl9252 <44651541+chl9252@users.noreply.github.com>
2018-11-02 19:24:32 +0500

    ДЗ 2
    Фон. Слайдшоу.

---
+++ docs/js/slideshow.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+	
+	var slideIndex = 0;
+	
+	showPhotos();
+
+	function showPhotos(){
+		var i = 0;
+		
+		var slides = $('.photo img');
+		
+
+		for ( i = 0; i < slides.length; i++){
+
+			$(slides[i]).hide();
+		}
+
+		slideIndex++;
+
+		
+
+		if(slideIndex > slides.length){
+			 slideIndex = 1;
+		}
+
+		$(slides[slideIndex-1]).show();
+
+
+
+		setTimeout(showPhotos,700);
+		
+
+	}
+});
